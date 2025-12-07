fede44fff28d6c89b9d43b3ef098ab981c224a9f
HNK <huynhnamkha1010@gmail.com>
2025-12-08 00:27:02 +0700

    chore(fe): test App

---
+++ frontend/src/App.test.tsx
@@ -0,0 +1,12 @@
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+
+describe("App Component", () => {
+  it("renders the hello message", () => {
+    render(<App />);
+
+    const heading = screen.getByText(/Hello Blogspace/i);
+
+    expect(heading).toBeInTheDocument();
+  });
+});
