8f521ab2e4cdd0cc0ad77a551cfafa119b34f299
HNK <huynhnamkha1010@gmail.com>
2025-12-20 14:30:12 +0700

    chore: configure add mongo module

--- backend/src/modules/user/user.module.ts
+++ backend/src/modules/user/user.module.ts
@@ -1,12 +1,18 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserRepository } from './user.repository';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserSchema, UserSchemaClass } from './entities/user.chema';
 
 @Module({
   imports: [
     // import modules, etc.
+    MongooseModule.forFeature([
+      { name: UserSchemaClass.name, schema: UserSchema },
+    ]),
   ],
   controllers: [],
   providers: [UserService, UserRepository],
+  exports: [UserService],
 })
 export class UserModule {}
