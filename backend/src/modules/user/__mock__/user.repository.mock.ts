fc414dc1e439ab532c3c273c0115260bf401c028
HNK <huynhnamkha1010@gmail.com>
2026-02-04 14:15:16 +0700

    chore: remove mock user

--- backend/src/modules/user/__mock__/user.repository.mock.ts
+++
@@ -1,5 +0,0 @@
-import { createMock, DeepMocked } from '@golevelup/ts-jest';
-import { UserRepository } from '../user.repository';
-
-export const mockUserRepository: DeepMocked<UserRepository> =
-  createMock<UserRepository>();
--- backend/src/modules/user/__mock__/user.service.mock.ts
+++
@@ -1,5 +0,0 @@
-import { createMock, DeepMocked } from '@golevelup/ts-jest';
-import { UserService } from '../user.service';
-
-export const mockUserService: DeepMocked<UserService> =
-  createMock<UserService>();
