2ed4b0f8ef7591a7aa447ef802130fa3a82b1796
HNK <huynhnamkha1010@gmail.com>
2026-02-04 13:26:28 +0700

    chore: remove mock file

--- backend/src/modules/file/__mock__/file.repository.mock.ts
+++
@@ -1,5 +0,0 @@
-import { createMock, DeepMocked } from '@golevelup/ts-jest';
-import { FileRepository } from '../file.repository';
-
-export const mockFileRepository: DeepMocked<FileRepository> =
-  createMock<FileRepository>();
--- backend/src/modules/file/__mock__/file.service.mock.ts
+++
@@ -1,5 +0,0 @@
-import { createMock, DeepMocked } from '@golevelup/ts-jest';
-import { FileService } from '../file.service';
-
-export const mockFileService: DeepMocked<FileService> =
-  createMock<FileService>();
